1be3da67d0fc78b0b4b1f5634a3f7bc813eebd30
MuhammadAqil25 <muhammadaqil2529@gmail.com>
2023-10-06 15:41:59 +0700

    Auto Select User

--- MiniProject/APITesting/main.js
+++ MiniProject/APITesting/main.js
@@ -38,20 +38,23 @@ const token = {
 // const token = '64b8408bd891b7ce40fc0d1fb6a7791b1fd7fe02e74abe3f9d9875bcc9986fa7'
 
 
-describe('GoRest API Testing ', function(){
+describe('GoRest API Testing ', function(){  
     it('001_Get All GoRest Users', async function() {
         const res = await request.get('/users')
         expect(res.body).have.jsonSchema(usersSchema)
-    })
+        expect(res.status).to.equal(200)
+    }) 
     it('002_Get a Single GoRest Users', async function() {
-        const res = await request.get('/users/5296989')
+        const userList = await request.get('/users')
+        const selectedUser = userList.body[1].id
+        const res = await request.get(`/users/${selectedUser}`)
         // console.log(res.body)
         expect(res.body).have.jsonSchema(userSchema)
     })
     it('003_Add New GoRest Users', async function() {
         const res = await request.post(`/users`).send({
             name: "Alexandre Christie",
-            email: "alex_chris6@wolf.example",
+            email: `alex_chris${Date.now()}@wolf.example`,
             gender: "Male",
             status: "active"
         }).set(token)
@@ -59,15 +62,19 @@ describe('GoRest API Testing ', function(){
         expect(res.body).have.jsonSchema(userSchema)
     })
     it('004_Update GoRest Users Details', async function() {
-        const res = await request.put(`/users/5297952`).send({
+        const userList = await request.get('/users')
+        const selectedUser = userList.body[1].id
+        const res = await request.put(`/users/${selectedUser}`).send({
             name: "Clone",
-            email: "Clone1@wolf.example",
+            email: `Clone${Date.now()}@wolf.example`,
         }).set(token)
         // console.log(res.body)
         expect(res.body).have.jsonSchema(userSchema)
     })
     it('005_Delete GoRest Users', async function() {
-        const res = await request.del(`/users/5297871`).set(token)
+        const userList = await request.get('/users')
+        const selectedUser = userList.body[1].id
+        const res = await request.del(`/users/${selectedUser}`).set(token)
         // console.log(res.body)
         expect(res.body).have.jsonSchema({})
     })
