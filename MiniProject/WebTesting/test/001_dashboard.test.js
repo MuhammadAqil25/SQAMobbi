48c399ab707761c17c95ae495fbb0c36f4650436
MuhammadAqil25 <muhammadaqil2529@gmail.com>
2023-10-10 20:28:33 +0700

    update data dashboard

--- MiniProject/WebTesting/test/001_dashboard.test.js
+++ MiniProject/WebTesting/test/001_dashboard.test.js
@@ -15,46 +15,46 @@ describe('Mizan Store Dashboard Web Testing', function() {
         driver.manage().window().maximize()
     })
 
-    // describe('Click Kategori on Navbar', function() {
-    //     it('Go to Kategori Page', async function() {
-    //         await dashboardPage.openPage()
-    //         await dashboardPage.clickNavBar(1)
-    //         const url = await driver.getCurrentUrl()
-    //         expect(url).to.equal('https://mizanstore.com/kategoriproduk/buku')
-    //     })
-    // })
-    // describe('Click POD on Navbar', function() {
-    //     it('Go to POD Page', async function() {
-    //         await dashboardPage.openPage()
-    //         await dashboardPage.clickNavBar(2)
-    //         const url = await driver.getCurrentUrl()
-    //         expect(url).to.equal('https://mizanstore.com/pencarian/hasil_pencarian?kata_pencarian=pod')
-    //     })
-    // })
-    // describe('Click SELF PUB on Navbar', function() {
-    //     it('Go to SELF PUB Page', async function() {
-    //         await dashboardPage.openPage()
-    //         await dashboardPage.clickNavBar(3)
-    //         const url = await driver.getCurrentUrl()
-    //         expect(url).to.equal('https://mizanstore.com/pencarian/hasil_pencarian?kata_pencarian=slpb')
-    //     })
-    // })
-    // describe('Click E Book on Navbar', function() {
-    //     it('Go to E Book Page', async function() {
-    //         await dashboardPage.openPage()
-    //         await dashboardPage.clickNavBar(4)
-    //         const url = await driver.getCurrentUrl()
-    //         expect(url).to.equal('https://mizanstore.com/ebook/index')
-    //     })
-    // })
-    // describe('Test Search on Navbar', function() {
-    //     it('Search item with Name Nusantara', async function() {
-    //         await dashboardPage.openPage()
-    //         await dashboardPage.searchProcess('Nusantara')
-    //         const url = await driver.getCurrentUrl()
-    //         expect(url).to.equal('https://mizanstore.com/pencarian/hasil_pencarian?kata_pencarian=Nusantara')
-    //     })
-    // })
+    describe('Click Kategori on Navbar', function() {
+        it('Go to Kategori Page', async function() {
+            await dashboardPage.openPage()
+            await dashboardPage.clickNavBar(1)
+            const url = await driver.getCurrentUrl()
+            expect(url).to.equal('https://mizanstore.com/kategoriproduk/buku')
+        })
+    })
+    describe('Click POD on Navbar', function() {
+        it('Go to POD Page', async function() {
+            await dashboardPage.openPage()
+            await dashboardPage.clickNavBar(2)
+            const url = await driver.getCurrentUrl()
+            expect(url).to.equal('https://mizanstore.com/pencarian/hasil_pencarian?kata_pencarian=pod')
+        })
+    })
+    describe('Click SELF PUB on Navbar', function() {
+        it('Go to SELF PUB Page', async function() {
+            await dashboardPage.openPage()
+            await dashboardPage.clickNavBar(3)
+            const url = await driver.getCurrentUrl()
+            expect(url).to.equal('https://mizanstore.com/pencarian/hasil_pencarian?kata_pencarian=slpb')
+        })
+    })
+    describe('Click E Book on Navbar', function() {
+        it('Go to E Book Page', async function() {
+            await dashboardPage.openPage()
+            await dashboardPage.clickNavBar(4)
+            const url = await driver.getCurrentUrl()
+            expect(url).to.equal('https://mizanstore.com/ebook/index')
+        })
+    })
+    describe('Test Search on Navbar', function() {
+        it('Search item with Name Nusantara', async function() {
+            await dashboardPage.openPage()
+            await dashboardPage.searchProcess('Nusantara')
+            const url = await driver.getCurrentUrl()
+            expect(url).to.equal('https://mizanstore.com/pencarian/hasil_pencarian?kata_pencarian=Nusantara')
+        })
+    })
     describe('Click Product Special Offer & Pre Order', function() {
         it('Go to Product Detail 1 Page', async function() {
             await dashboardPage.openPage()
